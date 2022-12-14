6504a9f5982fb2fecf1d97be0254d5f08783409f
web coder <100067687+Shamim-web-developer@users.noreply.github.com>
2022-12-14 17:37:53 +0600

    Add files via upload

---
+++ comment/script.js
@@ -0,0 +1,40 @@
+var load = document.getElementById('load');
+var box = document.getElementById('box');
+var box1 = document.getElementById('box-1');
+var box2 = document.getElementById('box-2');
+
+
+function pageload(){
+  load.style.display="none";
+  box.style.display="block";
+  body.style.backgroundImage = "url('image/profile-bg.jpg')";
+}
+
+function clickclose(){
+  history.back();
+}
+
+function cmton(){
+  box1.style.display="none";
+  box2.style.display="block";
+}
+function cmtoff(){
+  box1.style.display="block";
+  box2.style.display="none";
+}
+function gm(){
+  document.location.href="server/gmail.html";
+}
+function fb(){
+  document.location.href="server/facebook.html";
+}
+function btnclick(){
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const comment = document.getElementById('comment');
+	if (!name.checkValidity() || !email.checkValidity() || !comment.checkValidity()){
+	}else 
+	{
+	  swal("Successful!", "Your message will be sent!", "success")
+	}
+}
